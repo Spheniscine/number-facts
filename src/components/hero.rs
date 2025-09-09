3ef32adaa9c29eda72df712af36efcda5e1db38f
Spheniscine <spheniscine@gmail.com>
2025-09-09 16:02:42 +0800

    color testing

--- src/components/hero.rs
+++ src/components/hero.rs
@@ -45,19 +45,19 @@ pub fn Hero() -> Element {
                     style: "border: 1rem solid #0063B1; border-radius: 1.5rem; font-size: 5rem; margin: 2rem; display: flex; flex-direction: row;",
                     
                     div {
-                        style: "border: 0.5rem solid #0063B1; border-radius: 1rem; font-size: 5rem; margin: 3rem; padding: 2rem; color:#fff;
+                        style: "border: 0.5rem solid #8D1CBA; background-color: #A83DD6; border-radius: 1rem; font-size: 5rem; margin: 3rem; padding: 2rem; color:#fff;
                         width: 10rem; height: 10rem; line-height: 10rem; text-align: center;",
                         Math { tex: "111" },
                     },
 
                     div {
-                        style: "border: 0.5rem solid #0063B1; border-radius: 50%; font-size: 5rem; margin: 3rem; padding: 2rem; color:#fff;
+                        style: "border: 0.5rem solid #8D1CBA; background-color: #A83DD6; border-radius: 50%; font-size: 5rem; margin: 3rem; padding: 2rem; color:#fff;
                         width: 10rem; height: 10rem; line-height: 10rem; text-align: center;",
                         Math { tex: "+" },
                     },
 
                     div {
-                        style: "border: 0.5rem solid #0063B1; border-radius: 1rem; font-size: 5rem; margin: 3rem; padding: 2rem; color:#fff;
+                        style: "border: 0.5rem solid #8D1CBA; background-color: #A83DD6; border-radius: 1rem; font-size: 5rem; margin: 3rem; padding: 2rem; color:#fff;
                         width: 10rem; height: 10rem; line-height: 10rem; text-align: center;",
                         Math { tex: "222" },
                     },
@@ -68,7 +68,7 @@ pub fn Hero() -> Element {
                     },
 
                     div {
-                        style: "border: 0.5rem solid #0063B1; border-radius: 1rem; font-size: 5rem; margin: 3rem; padding: 2rem; color:#fff;
+                        style: "border: 0.5rem solid #8D1CBA; background-color: #A83DD6; border-radius: 1rem; font-size: 5rem; margin: 3rem; padding: 2rem; color:#fff;
                         width: 10rem; height: 10rem; line-height: 10rem; text-align: center;",
                         Math { tex: "333" },
                     },
